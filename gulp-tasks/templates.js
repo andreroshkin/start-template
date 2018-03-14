3734ab8770847d1e5628ae201a488788a3b5aa44
Andrey Eroshkin <andreroshkin@gmail.com>
2018-03-14 16:25:51 +0300

    error catch

--- gulp-tasks/templates.js
+++ gulp-tasks/templates.js
@@ -1,25 +1,29 @@
 module.exports = function (gulp, plugins) {
-  return () => {
-    gulp.src(['templates/*.twig'])
-    .pipe(plugins.data(function() {
-        return JSON.parse(plugins.fs.readFileSync('json/global.json'));
-    }))
-    .pipe(plugins.twig({
-        includes: [
-            'templates/partials/*.twig',
-        ],
-        getIncludeId: function(filePath) {
-            return plugins.path.relative('templates', filePath);
-        }
-    }))
-      .pipe(plugins.rename(function (path) {
-        path.extname = ".html"
+    return () => {
+      gulp.src(['templates/*.twig'])
+      .pipe(plugins.data(function() {
+          return JSON.parse(plugins.fs.readFileSync('json/global.json'));
       }))
-      .pipe(gulp.dest('./'))
-      .pipe(plugins.connect.reload());
-};
-};
-
-
-
-
+      .on('error', function (err) {
+          plugins.notify().write(err);
+          this.emit('end');
+      })
+      .pipe(plugins.twig({
+          includes: [
+              'templates/partials/*.twig',
+          ],
+          getIncludeId: function(filePath) {
+              return plugins.path.relative('templates', filePath);
+          }
+      }))
+      .on('error', function (err) {
+          plugins.notify().write(err);
+          this.emit('end');
+      })
+        .pipe(plugins.rename(function (path) {
+          path.extname = ".html"
+        }))
+        .pipe(gulp.dest('./'))
+        .pipe(plugins.connect.reload());
+  };
+  };
